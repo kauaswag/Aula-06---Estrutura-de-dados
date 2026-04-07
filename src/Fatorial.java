b22ad2c6de93374f1f9b0ea1b56de94cdb6849cd
kauaswag <kaualongui2017@gmail.com>
2026-04-07 19:54:55 -0300

    teste

---
+++ src/Fatorial.java
@@ -0,0 +1,22 @@
+public class Fatorial{
+    public static void main(String[] args) {
+        new Fatorial();
+    }
+
+    public Fatorial (){
+        System.out.println("Fatorial de 4: 4! = " + calculoIterativo(4));
+        System.out.println("Fatorial de 5: 5! = " + calculoIterativo(5));
+        System.out.println("Fatorial de 1: 1! = " + calculoIterativo(1));
+    }
+
+    public long calculoIterativo(int n) {
+        long resposta = n;
+        if (resposta < 0 ) return -1;
+        else if (resposta == 0 || resposta == 1) return 1;
+        for (int i = n - 1; i > 0; i--) {
+            resposta = resposta * i;
+
+        }
+        return resposta;
+    }
+}
